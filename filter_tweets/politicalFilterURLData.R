c2847023bf66b682ed22c2700193a192c5320d47
Lisa Friedland <lfriedlCS@gmail.com>
2018-03-07 20:15:27 -0500

    agh, what a mess. (Committing three versions of constructTweets so that I've saved a copy before deleting 2.)

--- filter_tweets/politicalFilterURLData.R
+++ filter_tweets/politicalFilterURLData.R
@@ -1,4 +1,5 @@
 source("classifyTweets.R")
+library(bit64)
 
 # Wrapper for calling classifyTweets on URL data.
 # Builds 1 model per input file. Assumes input file fits in memory.
@@ -9,8 +10,9 @@ source("classifyTweets.R")
 
 # inFile: a tsv.gz (or similar) containing columns tweet_id, tweet_text (and retweet_prefix, quote_of_user_name and quoted_text) and canonical_url.
 #   Repeats same tweet on multiple rows (assumed to be sequential!), once per URL. (Doesn't matter if it has only "external" URLs or also twitter_* ones.)
-# outFile: a tsv file with same fields as inFile + political_classifier_score and white_terms. See classifierThreshold for which lines.
-#   If classifierThreshold is NULL (implied if saveDebuggingColumnsRows is TRUE), keeps all rows. (Will have '.gz' appended to argument provided.)
+# outFile: a tsv file with same fields as inFile + political_classifier_score. See classifierThreshold for which lines.
+#   If classifierThreshold is NULL (implied if saveDebuggingColumnsRows is TRUE), keeps all rows and returns additional columns. 
+#   (File will have '.gz' appended to argument provided.)
 # keywordFile: used to define the training set, one term (word or phrase) per line. If a tweet contains one of these terms, it's ALWAYS kept (so, 
 #   be sure to put only high-precision terms in this file.)
 # modelFileOutStem: if non-NULL, we'll save two .rds files beginning with this path + prefix, to allow using same model (including doc->term mapping) on future tweets
@@ -23,12 +25,13 @@ filterURLDataUsingClassifier = function(inFile, outFile, keywordFile, modelFileO
     startTime = Sys.time()
     print(paste("Run started at", startTime))
     
-    # 1. Read data, change to tweet level (from tweet-url level), match against whitelist, tokenize, and build DocumentTermMatrix.
-    inData = fread(paste("zcat", inFile), sep = "\t", quote="")
+    # 1. Read data, change to tweet level (from tweet-url level).
+    inData = fread(paste("gzcat", inFile), sep = "\t", quote="'")
     tweetData = constructTweets(inData)    # contains tweet_id, complete_raw_text
     print(paste("Done constructing tweets at", Sys.time() - startTime))
-    
-    # 2. All the classification work! Construct training set, tokenize text, train classifier, get predictions from classifier.
+
+    # 2. All the text processing and classification work! Match against whitelist, tokenize text, build DocumentTermMatrix, 
+    # construct training set, train classifier, get predictions from classifier.
     tweetData = classifyTweets(keywordFile, tweetData = tweetData[, .(tweet_id, complete_raw_text)],  modelFileOutStem, startTime, goFastUseNaiveBayes, 
                                 noTraining=noTraining, loadVectorizer=loadVectorizer)
     
@@ -39,7 +42,7 @@ filterURLDataUsingClassifier = function(inFile, outFile, keywordFile, modelFileO
     if (saveDebuggingColumnsRows) {
         classifierThreshold = NULL
     } else {
-        # all we add to original is white_terms and political_classifier_score
+        # all we add to original is political_classifier_score
         colsWanted = setdiff(colsWanted, c("classifier_label", "complete_raw_text", "rest_of_text", "raw_classifier_score", "white_terms"))
     }
     if (! is.null(classifierThreshold)) {
@@ -52,90 +55,79 @@ filterURLDataUsingClassifier = function(inFile, outFile, keywordFile, modelFileO
     
 }
 
-# Very similar to the one above, but saves a slimmed-down file to use in prediction pipeline: one line per tweet, removes unneeded columns,
-# adds count columns.
-# Returns filename for filtered tweets.
-filterClassifyURLDataSaveAsTweets = function(inFile, outFileStem, keywordFile, modelFileOutStem=NULL, classifierThreshold=.8, saveDebuggingColumnsRows=FALSE,
-                                             goFastUseNaiveBayes=F, saveRawCountsFile=T) {
-
-    startTime = Sys.time()
-    print(paste("Run started at", startTime))
-    
-    # 1. Read data, change to tweet level (from tweet-url level), match against whitelist, tokenize, and build DocumentTermMatrix.
-    inData = fread(paste("zcat", inFile), sep = "\t", quote="")
-    tweetData = constructTweets(inData)    # contains tweet_id, complete_raw_text
-    print(paste("Done constructing tweets at", Sys.time() - startTime))
-    
-    # 2. All the classification work! Construct training set, tokenize text, train classifier, get predictions from classifier.
-    # tweetData columns added: classifier_label, white_terms, rest_of_text, raw_classifier_score, political_classifier_score
-    tweetData = classifyTweets(keywordFile, tweetData = tweetData[, .(tweet_id, complete_raw_text)],  modelFileOutStem, startTime, goFastUseNaiveBayes)
-    
-    # 3. Merge back with input data
-    firstRowEachTweet = !duplicated(inData$tweet_id)
-    dataWithScores = merge(inData[firstRowEachTweet,], tweetData, by="tweet_id")
-    
-    
-    # 4. Compute tweet counts per day and is_retweet (while we still have raw data allowing this).
-    dataWithScores[, is_retweet := !is.na(retweet_of_user_id) & !is.na(retweet_of_tweet_id)]
-    dataWithScores$unix_date = unclass(as.POSIXct(strptime(dataWithScores$tweet_date, format="%a %b %d %T +0000 %Y", tz="UTC")))
-    # How to account for London vs. U.S. time zones? Let's use midnight Eastern.
-    # (Alt options: could stick with London/UTC. Or use 5am Eastern? -- might work better wrt Alaska/Hawaii, but would be weirder to explain to people.)
-    dataWithScores$date_eastern = substr(as.POSIXlt(dataWithScores$unix_date, origin="1970-01-01"), 1, 10)
-    rawCountsPerPersonDay = dataWithScores[, .(num_tweets_by_user_today = .N), by=.(user_id, date_eastern)]
-    if (saveRawCountsFile) {  # need to save separately, otherwise people w/o political tweets won't have their num tweets recorded.
-        countsFile = paste0(outFileStem, ".counts.csv")
-        fwrite(rawCountsPerPersonDay, file=countsFile)
-        system(paste("gzip -f", countsFile))
-    }
-    dataWithScores = merge(dataWithScores, rawCountsPerPersonDay, by=c("user_id", "date_eastern"))
-    # no need for this here, since we need to count it later across files 
-    #if (! is.null(classifierThreshold)) {
-    #    politicalCountsPerPersonDay = dataWithScores[political_classifier_score >= classifierThreshold, 
-    #                                                 .(num_political_tweets_by_user_today = .N), by=.(user_id, date_eastern)]
-    #    dataWithScores = merge(dataWithScores, politicalCountsPerPersonDay, by=c("user_id", "date_eastern"), all.x=T)
-    #}
-    
-    # 5. Apply threshold for filtering
-    if (saveDebuggingColumnsRows) {
-        classifierThreshold = NULL
-        additionalColsWanted = c("classifier_label", "rest_of_text", "raw_classifier_score")
-    } else {
-        # all we add to original is white_terms and political_classifier_score
-        additionalColsWanted = c()
-    }
-    if (! is.null(classifierThreshold)) {
-        print(paste("Keeping the", sum(dataWithScores$political_classifier_score >= classifierThreshold), "of", nrow(dataWithScores), 
-                    "lines with a classifier score >=", classifierThreshold))
-        dataWithScores = dataWithScores[political_classifier_score >= classifierThreshold,]
-    }
-    
-    # 4. Get rid of extra columns, save
-    colsWanted = c("tweet_id", "user_id", "tweet_date", "date_eastern",
-                   "candidate_interaction",  "political_classifier_score", 
-                   "white_terms", "complete_raw_text", additionalColsWanted)
-    outFile = paste0(outFileStem, ".tsv")
-    fwrite(dataWithScores[, colsWanted, with=F], file=outFile, sep="\t", quote=F)
-    system(paste("gzip -f", outFile))
-    
-    return(paste0(outFile, ".gz"))
-    
-}
 
 # Gathers text + URLs for each tweet
 # inData: a data.table (straight from the inFile)
 # returns: a data.table with columns tweet_id and complete_raw_text 
-constructTweets = function(inData) {
+constructTweets = function(inData, newWay=T) {
+    setkey(inData, tweet_id)
     
-    # be a little more precise with URLs than before: attach orig/retweeted ones [expansions] to tweet_text, but quoted ones to quoted_text.
-    tweetURLs_orig = inData[where_url_found != 'quoted', .(orig_urls = paste(canonical_url, collapse=" ")), by = tweet_id]
-    tweetURLs_quoted = inData[where_url_found == 'quoted', .(quoted_urls = paste(canonical_url, collapse=" ")), by = tweet_id]
+    # Replace shortened URLs with expanded ones. 
+    if (newWay) {
+        require("stringr")
     
-    # add new urls columns to inData
-    tweetData = merge(inData, tweetURLs_orig, all.x=T)  
-    tweetData = merge(tweetData, tweetURLs_quoted, all.x=T)  
-    # that produced NAs. Change to empty strings.
-    tweetData[is.na(orig_urls), orig_urls := '']
-    tweetData[is.na(quoted_urls), quoted_urls := '']
+        tweetData = inData[canonical_url != '', tweet_text:=str_replace(tweet_text, fixed(shortened_url), canonical_url)]
+        tweetData = tweetData[canonical_url != '', quoted_text:=str_replace(quoted_text, fixed(shortened_url), canonical_url)]
+        
+    firstRowEachTweet = !duplicated(tweetData$tweet_id)
+        firstRowEachTweet = !duplicated(tweetData$tweet_id)
+        tweetData$quoting = ifelse(tweetData$quote_of_user_name == '', '', paste0("[QTG @", tweetData$quote_of_user_name, "]"))
+        
+        tweetData = tweetData[firstRowEachTweet, .(tweet_id, complete_raw_text = paste(retweet_prefix, tweet_text, 
+                                                                              quoting, quoted_text))]
+        return(tweetData)
+    } else if (T) {
+        swapIn = function(text, from, to) {
+            if (!is.null(to[[1]]) && !is.null(from[[1]]) && length(to) > 0 && length(from) > 0
+                && to[[1]] != '' && from[[1]] != '') {
+                namedVector = unlist(to)
+                names(namedVector) = unlist(from)
+                return(str_replace_all(text, namedVector))
+            } else {
+                return(text)
+            }
+        }
+        tweetURLs_orig = inData[where_url_found != 'quoted', 
+                              .(orig_shortened = list(.SD[, shortened_url]), orig_canon = list(.SD[, canonical_url])),
+                              by = tweet_id, .SDcols=c("shortened_url", "canonical_url")]
+        tweetURLs_quoted = inData[where_url_found == 'quoted', 
+                                  .(quoted_shortened = list(.SD[, shortened_url]), quoted_canon = list(.SD[, canonical_url])),
+                                  by = tweet_id, .SDcols=c("shortened_url", "canonical_url")]
+        tweetData = merge(inData, tweetURLs_orig, all.x=T)  
+        tweetData = merge(tweetData, tweetURLs_quoted, all.x=T) 
+        # merges produce NULLs, but they're inside lists. Handle them during swapIn().
+        # that produced NAs. Change to empty strings.
+        #tweetData[is.null(orig_shortened[[1]]), `:=`(orig_shortened = list(""), orig_canon = list(""))]
+        #tweetData[is.null(quoted_shortened[[1]]), `:=`(quoted_shortened = list(""), quoted_canon = list("")) ]  # these give warnings but seem to work
+        
+        
+        onePerTweet = tweetData[!duplicated(tweet_id),]
+        onePerTweet[, updated_tweet_text := mapply(swapIn, tweet_text, orig_shortened, orig_canon)]
+        onePerTweet[, updated_quoted_text := mapply(swapIn, quoted_text, quoted_shortened, quoted_canon)]
+        
+        onePerTweet[, quoting := ifelse(quote_of_user_name == "", "", paste0("[QTG @", quote_of_user_name, "]"))]
+        # text: Paste back together the RT prefix, the tweet_text, the quoting marker, and the quoted_text
+        onePerTweet = onePerTweet[, .(tweet_id, raw_text = paste(retweet_prefix, updated_tweet_text, 
+                                                                              quoting, updated_quoted_text))]
+        # The whole reason we expanded URLs is because the t.co versions are meaningless. Delete those.
+        onePerTweet[, complete_raw_text := gsub("(?<!\\w)https?\\://t\\.co/\\S+", "", raw_text, perl=T)]
+        
+        return(onePerTweet[, .(tweet_id, complete_raw_text)])
+        
+        
+    } else {  # old way
+    
+        # be a little more precise with URLs than before: attach orig/retweeted ones [expansions] to tweet_text, but quoted ones to quoted_text.
+        tweetURLs_orig = inData[where_url_found != 'quoted', .(orig_urls = paste(canonical_url, collapse=" ")), by = tweet_id]
+        tweetURLs_quoted = inData[where_url_found == 'quoted', .(quoted_urls = paste(canonical_url, collapse=" ")), by = tweet_id]
+        
+        # add new urls columns to inData
+        tweetData = merge(inData, tweetURLs_orig, all.x=T)  
+        tweetData = merge(tweetData, tweetURLs_quoted, all.x=T)  
+        # that produced NAs. Change to empty strings.
+        tweetData[is.na(orig_urls), orig_urls := '']
+        tweetData[is.na(quoted_urls), quoted_urls := '']
+    
     
     # text: Paste back together the RT prefix, the tweet_text + orig_urls, the quoting marker, and the quoted_text + quoted_urls.
     firstRowEachTweet = !duplicated(tweetData$tweet_id)
@@ -146,8 +138,9 @@ constructTweets = function(inData) {
                                                                        quoting, quoted_text, quoted_urls))]
     # The whole reason we expanded URLs is because the t.co versions are meaningless. Delete those.
     tweetData$complete_raw_text = gsub("(?<!\\w)https?\\://t\\.co/\\S+", "", tweetData$raw_text, perl=T)
-    
     return(tweetData[, .(tweet_id, complete_raw_text)])
+    }
+    
     
 }
 
